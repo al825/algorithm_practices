fd881d4864213fa40ea05673cedcc13e7157b196
al825 <anqi.liang@yale.edu>
2016-12-19 23:24:15 -0800

    Make quicksort work.

--- quicksort.py
+++ quicksort.py
@@ -1,49 +1,46 @@
 """Implement quick sort in Python.
 Input a list.
 Output a sorted list."""
-def quicksort(array):
-    print ("input array:")
-    print(array)
-    if len(array) == 1:
+def quicksort(array, debug=False):
+    if debug:     
+        print ("input array:")
+        print(array)
+    # input not a list
+    if not isinstance(array, list):
+        return [array]
+    # input is a list of length 1
+    elif len(array) == 1:
         return array
+    # input is a list of length 20
+    elif len(array) == 2:
+        return [min(array), max(array)]
+    # convention: use the last element as pivot    
     pivot = array[-1]
     pivot_index = len(array) - 1
     i = 0
     while i < pivot_index: 
-        print (pivot_index)
-        print (array)
+        if debug:
+            print (pivot_index)
+            print (array)
         if array[i] > pivot:
             temp_val = array[i]
-            array[i:pivot_index] = array[i+1: pivot_index+1]
-            array[pivot_index] = temp_val
+            array[i] = array[pivot_index-1] # move the element before the pivot to the ith position
+            array[pivot_index-1] = pivot # move pivot one step forward
+            array[pivot_index] = temp_val # move the ith element after the pivot
             pivot_index -= 1
         else:
             i += 1
+    # pivot is the largest in the array
     if pivot_index == len(array)-1:
-        quicksort(array[:pivot_index])
-        if isinstance(array[:pivot_index]):
-            return larray[:pivot_index] + [array[pivot_index]]
-        else:
-            return [array[:pivot_index]] + [array[pivot_index]]        
+        return quicksort(array[:pivot_index]) + [pivot]
+    # pivot is the smallest in the array
     elif pivot_index == 0:
-        quicksort(array[pivot_index+1:])
-        if isinstance(array[pivot_index+1:], list):
-            return array[pivot_index] + [(array[pivot_index+1:])]
-        else:
-            return list(array[pivot_index]) + list(array[pivot_index+1:
+        return [pivot] + quicksort(array[pivot_index+1:])
     else: 
-        quicksort(array[:pivot_index])
-        quicksort(array[pivot_index+1:])
-        if isinstance(array[:pivot_index], list):
-            if isinstance(array[pivot_index+1:], list):
-                return array[:pivot_index] + [array[pivot_index]] + array[pivot_index+1:]
-            else:
-                return array[:pivot_index] + [array[pivot_index]] + [array[pivot_index+1:]]
-        else:
-            if isinstance(array[pivot_index+1:], list):
-                return [array[:pivot_index]] + [array[pivot_index]] + array[pivot_index+1:]
-            else:
-                return [array[:pivot_index]] + [array[pivot_index]] + [array[pivot_index+1:]]
+        return quicksort(array[:pivot_index]) + [pivot] + quicksort(array[pivot_index+1: ])
+
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
 print quicksort(test)
+
+
